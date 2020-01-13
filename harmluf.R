d8d4e9bfe2005fc98497e8e6d3298dc77f55d4e0
pacoraggio <pacoraggio@gmail.com>
2020-01-13 11:41:26 +0000

    pre and post 1995 analysis added

--- harmluf.R
+++ harmluf.R
@@ -1,46 +1,47 @@
 #1. Across the United States, which types of events (as indicated in the EVTYPE 
 #  variable) are most harmful with respect to population health?
+
+# To address this question I am considering the reported casualties 
+# (FATALITIES and INJURIES variables of the dataset). 
+
 rm(list = ls())
 load("workingdata.RData")
 
 library(ggplot2)
 library(dplyr)
 
+# df.harmf <- df.wdata[df.wdata$FATALITIES != 0 | df.wdata$INJURIES != 0,]
 
-df.harmf <- df.wdata[df.wdata$FATALITIES != 0 | df.wdata$INJURIES != 0,]
+sum(df.wdata$FATALITIES)
+sum(df.wdata$INJURIES)
+# names(df.harmf)
+# ragg <- aggregate(FATALITIES ~ EVTYPE, data = df.harmf, sum)
+# class(ragg)
+# sort(ragg$FATALITIES, decreasing = TRUE)[1:10]
 
-sum(df.harmf$FATALITIES)
-sum(df.harmf$INJURIES)
-names(df.harmf)
-
-ragg <- aggregate(FATALITIES ~ EVTYPE, data = df.harmf, sum)
-class(ragg)
-
-sort(ragg$FATALITIES, decreasing = TRUE)[1:10]
-
-rgb <- df.harmf %>% group_by(EVTYPE) %>%
+df.casualties <- df.wdata %>% group_by(EVTYPE) %>%
     summarise(sum_fatalities = sum(FATALITIES), sum_injuries = sum(INJURIES)) %>%
     mutate(total_sum = sum_fatalities + sum_injuries)
 
+head(df.casualties)
 
-top10Harm <- as.data.frame(rgb[order(rgb$total_sum, decreasing = TRUE)[1:10], ])
+df.top10Harm <- as.data.frame(df.casualties[order(df.casualties$sum_fatalities, 
+                                                  decreasing = TRUE)[1:10], ])
 
 library(reshape)
-rs <- melt(top10Harm[,c("EVTYPE", "sum_fatalities","sum_injuries")], id.vars = 1)
+df.top10Harmplot <- melt(df.top10Harm[,c("EVTYPE", 
+                                         "sum_fatalities",
+                                         "sum_injuries",
+                                         "total_sum")], id.vars = 1)
 
-head(rs)
 windows()
-ggplot(rs, aes(x = reorder(EVTYPE,value), y = value)) +
+ggplot(df.top10Harmplot, aes(x = reorder(EVTYPE,value), y = value)) +
     geom_bar(aes(fill = variable), stat = "identity", position = "dodge") +
     coord_flip()
 
-
-top10Harm
-
-library(ggplot2)
-windows()
-ggplot(rs, aes(reorder(EVTYPE, value), value)) +
+ggplot(df.top10Harmplot, aes(reorder(EVTYPE, value), value)) +
     geom_col(aes(fill = variable)) +
+    facet_grid(~variable) + 
     coord_flip()
 
 head(top10Harm)
@@ -48,6 +49,32 @@ head(top10Harm)
 library(lubridate)
 head(df.harmf)
 
+## from 1995
+
+t <- df.wdata[df.wdata$YEAR > 1995,]
+head(t)
+
+df.casualties <- df.wdata[df.wdata$YEAR > 1995,] %>% group_by(EVTYPE) %>%
+    summarise(sum_fatalities = sum(FATALITIES), sum_injuries = sum(INJURIES)) %>%
+    mutate(total_sum = sum_fatalities + sum_injuries)
+
+head(df.casualties)
+
+df.top10Harm <- as.data.frame(df.casualties[order(df.casualties$sum_fatalities, 
+                                                  decreasing = TRUE)[1:10], ])
+
+
+df.top10Harmplot <- melt(df.top10Harm[,c("EVTYPE", 
+                                         "sum_fatalities",
+                                         "sum_injuries",
+                                         "total_sum")], id.vars = 1)
+
+windows()
+ggplot(df.top10Harmplot, aes(x = reorder(EVTYPE,value), y = value)) +
+    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") +
+    coord_flip()
+
+### Temporal analysis - for future works
 kY <- df.harmf %>% 
     group_by(YEAR) %>%
     summarise(sf = sum(FATALITIES), 
